28564dc2bdcecae764c6c93e175ddeb50d1fc29c
jonnynavarro <jonnynavarro@gmail.com>
2022-07-23 02:43:38 -0300

    Todos os arquivos

---
+++ init.lua
@@ -0,0 +1,17 @@
+-- Settings & Keys
+require 'settings'
+require 'keymaps'
+
+
+-- Plugins
+require 'plugins.plugins'
+require 'plugins.dashboard'
+require 'plugins.feline'
+require 'plugins.bufferline'
+require 'plugins.tree'
+require 'plugins.telescope'
+require 'plugins.treesitter'
+--require 'plugins.lsp-config'
+--require 'plugins.cmp'
+require 'plugins.autopairs'
+require 'coc'
